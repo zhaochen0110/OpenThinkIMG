4b6bfe0e0e846dea0d875fc320e2a95275a37797
Song Mingyang <hitsmy@qq.com>
2024-11-21 19:12:13 +0800

    initial commit

---
+++ LLaVA-Plus-Codebase/serve/scripts/run_test_dino_message.sh
@@ -0,0 +1,17 @@
+# environment variables
+source ~/.bashrc
+source ~/anaconda3/bin/activate llava_plus
+
+cd /mnt/petrelfs/songmingyang/code/reasoning/ref/LLaVA-Plus-Codebase/serve
+
+export SLURM_JOB_ID=3273170
+unset SLURM_JOB_ID     
+
+gpus=0
+cpus=2
+quotatype="auto"
+
+OMP_NUM_THREADS=8 srun --partition=MoE --job-name="LM_LCS558K" --mpi=pmi2 --gres=gpu:${gpus}  -n1 --ntasks-per-node=1 -c ${cpus} --kill-on-bad-exit=1 --quotatype=${quotatype}  \
+-w SH-IDCA1404-10-140-54-5 \
+python grounding_dino_test_message.py \
+--controller-address http://10.140.54.5:20001
