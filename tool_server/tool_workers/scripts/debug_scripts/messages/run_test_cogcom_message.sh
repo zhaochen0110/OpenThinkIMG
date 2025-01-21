9bf8a39410383d94ad18b690033b108ebed3bf66
hychaochao <hyz.chaochao@gmail.com>
2025-01-21 11:34:09 +0800

    add select_subplots_worker

--- tool_server/tool_workers/scripts/debug_scripts/messages/run_test_cogcom_message.sh
+++ tool_server/tool_workers/scripts/debug_scripts/messages/run_test_cogcom_message.sh
@@ -1,8 +1,8 @@
-cd /mnt/petrelfs/haoyunzhuo/mmtool/Tool-Factory/tool_server/tool_workers/restructure_worker/test_cases   
+cd /mnt/petrelfs/haoyunzhuo/mmtool/Tool-Factory/tool_server/tool_workers/online_workers/test_cases
 
 gpus=0
 cpus=2
 quotatype="auto"
 
-OMP_NUM_THREADS=8 srun --partition=MoE --job-name="test-sam" --mpi=pmi2 --gres=gpu:${gpus}  -w SH-IDCA1404-10-140-54-119  -n1 --ntasks-per-node=1 -c ${cpus} --kill-on-bad-exit=1 --quotatype=${quotatype}  \
-python ocr_test_message.py --send_image \
+OMP_NUM_THREADS=8 srun --partition=MoE --job-name="test-subplot" --mpi=pmi2 --gres=gpu:${gpus}  -w SH-IDCA1404-10-140-54-119  -n1 --ntasks-per-node=1 -c ${cpus} --kill-on-bad-exit=1 --quotatype=${quotatype}  \
+python test_subplot_messages.py --send_image \
--- tool_server/tool_workers/scripts/debug_scripts/start_cogcom_grounding.sh
+++ tool_server/tool_workers/scripts/debug_scripts/start_cogcom_grounding.sh
@@ -1,13 +1,13 @@
-cd /mnt/petrelfs/haoyunzhuo/mmtool/Tool-Factory/tool_server/tool_workers/restructure_worker
+cd /mnt/petrelfs/haoyunzhuo/mmtool/Tool-Factory/tool_server/tool_workers/online_workers
 
 
 OMP_NUM_THREADS=8 srun \
  --partition=MoE \
  --mpi=pmi2 \
- --job-name=line \
+ --job-name=subplot \
  -c 32 \
  -w SH-IDCA1404-10-140-54-119 \
  --ntasks-per-node=1 \
  --kill-on-bad-exit=1 \
  --quotatype=reserved \
- python ./DrawVerticalLineByX_worker.py
+ python ./zoomInsubfigure_worker.py
