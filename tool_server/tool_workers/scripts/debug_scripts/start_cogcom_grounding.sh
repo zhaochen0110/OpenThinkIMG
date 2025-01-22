70922751d91aa5304aec7a34c543f8219c048d59
hychaochao <hyz.chaochao@gmail.com>
2025-01-22 18:37:42 +0800

    add ocr worker

--- tool_server/tool_workers/scripts/debug_scripts/start_cogcom_grounding.sh
+++ tool_server/tool_workers/scripts/debug_scripts/start_cogcom_grounding.sh
@@ -4,10 +4,10 @@ cd /mnt/petrelfs/haoyunzhuo/mmtool/Tool-Factory/tool_server/tool_workers/online_
 OMP_NUM_THREADS=8 srun \
  --partition=MoE \
  --mpi=pmi2 \
- --job-name=subplot \
+ --job-name=ocr \
  -c 32 \
  -w SH-IDCA1404-10-140-54-119 \
  --ntasks-per-node=1 \
  --kill-on-bad-exit=1 \
  --quotatype=reserved \
- python ./zoomInsubfigure_worker.py
+ python ./ocr_worker.py
