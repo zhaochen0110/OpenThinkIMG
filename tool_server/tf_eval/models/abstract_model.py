5fc1333e4144955550bd35129b82890e4201e026
Mingyang Song <1404838873@qq.com>
2025-01-22 14:42:23 +0800

    Merge branch 'tool_server_develop' into main

--- tool_server/tf_eval/models/abstract_model.py
+++ tool_server/tf_eval/models/abstract_model.py
@@ -1,41 +1,13 @@
 import abc
-import hashlib
-import json
-import os
 from typing import List, Optional, Tuple, Type, TypeVar, Union
 
-from loguru import logger as eval_logger
-from tqdm import tqdm
-from gradio.helpers import Examples
-import argparse
-import base64
-from collections import defaultdict
-import copy
-import datetime
+
 from functools import partial
-import json
-import os
-import torch
-from pathlib import Path
-import cv2
-import numpy as np
-import re
-import time
-from io import BytesIO
-from PIL import Image
-from PIL import Image as _Image  # using _ to minimize namespace pollution
-
-import gradio as gr
-from gradio import processing_utils, utils
-from gradio_client import utils as client_utils
-
-import requests
 
 from ...utils.utils import *
 from ...utils.server_utils import *
 from ..tool_inferencer.dynamic_batch_manager import DynamicBatchItem
 
-import pycocotools.mask as mask_util
 import uuid
 
 from ..utils.log_utils import get_logger
