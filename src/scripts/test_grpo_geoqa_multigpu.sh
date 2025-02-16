e4627e2984a063abe9735a43359d43eaea30aa3d
Liang Chen <leo.liang.chen@outlook.com>
2025-02-16 21:07:37 +0800

    Update test_grpo_geoqa_multigpu.sh

--- src/scripts/test_grpo_geoqa_multigpu.sh
+++ src/scripts/test_grpo_geoqa_multigpu.sh
@@ -7,7 +7,7 @@ output_path=${r1_v_path}/output/train@geo170k/eval/res@checkpoint-30.json
 prompt_path=${r1_v_path}/src/eval/prompts/geoqa_test_prompts.jsonl
 gpu_ids=0,1,2,3,4,5,6,7
 
-python src/eval/test_qwen2vl_geoqa.py \
+python src/eval/test_qwen2vl_geoqa_multigpu.py \
     --model_path ${model_path} \
     --batch_size ${batch_size} \
     --output_path ${output_path} \
